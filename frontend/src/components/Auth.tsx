69664a4a446e6ded32bee5f966137eebdc19f7ec
PiyushTanay01 <piyushtanay434@gmail.com>
2024-06-22 01:55:30 +0530

    module

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, useState } from "react";
 import { Link,useNavigate } from "react-router-dom"
-import { SignupInput } from "@piyushtanay/medium-common";
+import { SignupInput } from "@piyushtanay/medium-common1";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
 
